d8ac2bcc845eee5c9ba163d91e6823372148ffb4
cohessel <larissahessel@outlook.com>
2020-09-23 22:57:54 -0300

    app com bug nas imagens ;-;(arrumar isso dps)

--- programacao-app/app/src/main/java/com/example/VidaeImagem/MainActivity.kt
+++ programacao-app/app/src/main/java/com/example/VidaeImagem/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.VidaeImagem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var cliques = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun contar(component:View) {
+        cliques++
+
+        text_view.text = "Cliques: $cliques"
+
+        if(cliques == 5) {
+            foto.setImageDrawable(getDrawable(R.mipmap.titi_emoji))
+        }
     }
 }
